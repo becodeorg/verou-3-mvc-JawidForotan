a253fdc52d706462cbdd4826c0678235c11131fb
Jawid <jawidforotan100@gmail.com>
2022-03-21 10:58:16 +0100

    DB class is created and config file is hidden in gitignore file.

---
+++ DB/database.php
@@ -0,0 +1,34 @@
+<?php
+require"config.php";
+class Database
+{
+    private string $host;
+    private string $user;
+    private string $password;
+    private string $dbname;
+
+    public PDO $connection;
+
+    public function __construct(string $host, string $user, string $password, string $dbname)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->dbname = $dbname;
+    }
+
+    public function connect(): void
+    {
+        $dsn = "mysql:host={$this->host}; dbname={$this->dbname}";
+        try{
+            $this->connection = new PDO($dsn, $this->user, $this->password);
+            echo "Success";
+        }catch(PDOException $error){
+            $errorMassage = "Database error";
+            $errorMassage .= $error->getMessage();
+            echo $errorMassage;
+            exit();
+        }
+    }
+}
+?>
